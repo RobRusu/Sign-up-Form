1e04b9cf636f4b2ce89284bbaa0703e33d6dcb78
RobRusu <142660890+RobRusu@users.noreply.github.com>
2023-12-13 11:26:35 +0000

    logic added for password match and styling for required fields

---
+++ script.js
@@ -0,0 +1,15 @@
+const pw = document.querySelector('#password');
+const confirmpw = document.querySelector('#confirmPw');
+const check = document.querySelector('.pwCheck');
+
+function pwCheck (){
+  if (pw.value !== confirmpw.value){
+    check.textContent = '*Passwords do not match';
+  } else {
+    check.textContent = '';
+  }
+}
+
+confirmpw.addEventListener('input', pwCheck)
+pw.addEventListener('input', pwCheck)
+
